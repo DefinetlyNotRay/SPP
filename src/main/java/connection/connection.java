3ac3a82f4b324ab323edb14592406a433473720f
DefinetlyNotRay <edgarmasstudio@gmail.com>
2023-10-28 21:11:54 +0700

    Initial commit

---
+++ src/main/java/connection/connection.java
@@ -0,0 +1,36 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connection {
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/spp";
+        String user = "root";
+        String password = "";
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Error connecting to database: " + e.getMessage());
+        }
+
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection c = getConnection();
+            if (c != null) {
+                System.out.println(String.format("Connected to database %s successfully.", c.getCatalog()));
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
